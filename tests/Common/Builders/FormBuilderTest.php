dae0698a4cfccb832954037aee43010372f87cf9
Fabio Mattei <burattino@gmail.com>
2018-05-28 03:16:20 +0200

    addig test fo form builder

---
+++ tests/Common/Builders/FormBuilderTest.php
@@ -0,0 +1,23 @@
+<?
+
+/**
+*  Testing the BaseForm class
+*
+*  @author Fabio Mattei
+*/
+class FormBuilderTest extends PHPUnit_Framework_TestCase{
+	
+  /**
+  * Just check if the YourClass has no syntax error 
+  *
+  * This is just a simple check to make sure your library has no syntax error. This helps you troubleshoot
+  * any typo before you even use this library in a real project.
+  *
+  */
+  public function testIsThereAnySyntaxError(){
+	  $form = new Firststep\Common\Builders\FormBuilder;
+	  $this->assertTrue(is_object($form));
+	  unset($form);
+  }
+  
+}
