b9676c6771d9e05eae784ce79d21cea6dae1bb54
Fabio Mattei <burattino@gmail.com>
2018-06-30 11:02:54 +0200

    making room for admin functionalities

---
+++ src/Controllers/Admin/FormCreateTable.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Firststep\Controllers\Admin;
+
+use Firststep\Common\Controllers\Controller;
+
+/**
+ * 
+ */
+class FormCreateTable extends Controller {
+	
+	function __construct($argument) {
+		# code...
+	}
+
+}
---
+++ src/Controllers/Admin/FormVersionList.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Firststep\Controllers\Admin;
+
+use Firststep\Common\Controllers\Controller;
+
+/**
+ * 
+ */
+class FormVersionList extends Controller {
+	
+	function __construct($argument) {
+		# code...
+	}
+
+}
