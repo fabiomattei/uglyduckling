cc4d75b55db9b98c977f9a0d86870d60918f4bde
Fabio Mattei <burattino@gmail.com>
2018-07-09 20:00:27 +0200

    updating bootstrap

--- src/Templates/application.php
+++ src/Templates/application.php
@@ -95,8 +95,8 @@
 	  </footer>
 
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
-    <script src="<?php echo BASEPATH; ?>assets/lib/jquery/jquery.js">"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
+    <script src="<?php echo BASEPATH; ?>assets/lib/jquery/jquery-3.3.1.min.js">"></script>
+	<script src="<?php echo BASEPATH; ?>assets/lib/popper.min.js">"></script>
     <script src="<?php echo BASEPATH; ?>assets/js/bootstrap.min.js"></script>
 
     <?php echo isset($this->addToFoot) ? $this->addToFoot : '' ?>
