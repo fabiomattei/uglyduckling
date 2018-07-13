2044fd699a2c0a3eb72366f709316a5363314d9e
Fabio Mattei <burattino@gmail.com>
2018-07-13 11:08:34 +0200

    testing parameters parser class

---
+++ tests/Common/Json/JsonParametersParserTest.php
@@ -0,0 +1,41 @@
+<?php 
+
+/**
+*  Testing the JsonParametersParser class
+*
+*  @author Fabio Mattei
+*/
+class JsonParametersParserTest extends PHPUnit_Framework_TestCase {
+	
+	private $json = '{
+  "request": {
+    "parameters": [
+      { "type":"int", "validation":"required|int", "name":"1" }
+    ]
+  }
+}';
+
+    /**
+     * Just check if the JsonParametersParser has no syntax error 
+     *
+     * This is just a simple check to make sure your library has no syntax error. This helps you troubleshoot
+     * any typo before you even use this library in a real project.
+     *
+     */
+    public function testIsThereAnySyntaxError(){
+        $parser = new Firststep\Common\Json\JsonParametersParser;
+	    $this->assertTrue( is_object( $parser ) );
+        unset( $parser );
+    }
+	
+    public function testOnceWeSetRequestItGetsTheAction(){
+		$resource = json_decode($this->json);
+		$parameters = Firststep\Common\Json\JsonParametersParser::parseResourceForParametersValidationRoules($resource);
+        $this->assertNotEmpty($parameters['rules']);
+		$this->assertNotEmpty($parameters['filters']);
+		$this->assertCount(1, $parameters['rules']);
+		$this->assertCount(1, $parameters['filters']);
+		$this->assertSame('required|int', $parameters['rules'][1]);
+		$this->assertSame('trim', $parameters['filters'][1]);
+    }
+}
