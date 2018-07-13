93e71ec79a78433875fcb6a76e07dee5c5b65f78
Fabio Mattei <burattino@gmail.com>
2018-07-13 20:34:08 +0200

    moving files

---
+++ src/Common/Json/JsonBlockTableParser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Firststep\Common\Json;
+
+use Firststep\Common\Builders\FormBuilder;
+
+/**
+ * JsonLoader makes an index of all available resources and load the 
+ * resource if needed
+ */
+class JsonBlockTableParser {
+	
+	__construct() {
+		$this->formBuilder = new FormBuilder();
+	}
+	
+	public static function parse( $resource, $entity ) {
+		$this->formBuilder->setFormStructure($resource);
+		$this->formBuilder->setEntity($resource);
+		return $this->formBuilder->createForm();
+	}
+	
+}
