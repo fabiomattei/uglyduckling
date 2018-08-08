0f3f04acfc0e869bba403a37e796273349120105
Fabio Mattei <burattino@gmail.com>
2018-08-09 01:29:38 +0200

    col width

---
+++ src/Common/Blocks/ColWidth.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Firststep\Common\Blocks;
+
+use Firststep\Common\Blocks\BaseBlock;
+
+class ColWidth extends BaseBlock {
+	
+	const EXTRA_SMALL = 'col-';         // < 576px
+	const SMALL       = 'col-sm-';      // >= 576px
+	const MEDIUM      = 'col-md-';      // >= 768px
+	const LARGE       = 'col-lg-';      // >= 992px
+	const EXTRA_LARGE = 'col-xl-';      // >= 1200px
+	
+}
