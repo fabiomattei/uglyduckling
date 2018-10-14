56fc3f0a9057f9deeae40dc1fc7ad202ff9fdee1
Fabio Mattei <burattino@gmail.com>
2018-10-14 15:01:50 +0200

    starting to implement edit and info

--- src/Controllers/Office/Document/DocumentEdit.php
+++ src/Controllers/Office/Document/DocumentEdit.php
@@ -2,15 +2,86 @@
 
 namespace Firststep\Controllers\Office\Document;
 
-use Firststep\Common\Controllers\Controller;
+use Firststep\Common\Controllers\ManagerDocumentSenderController;
+use Firststep\Templates\Blocks\Sidebars\AdminSidebar;
+use Firststep\Common\Json\JsonBlockFormParser;
+use Firststep\Common\Blocks\StaticTable;
+use Firststep\Common\Blocks\Button;
+use Firststep\Common\Router\Router;
+use Firststep\Common\Database\QueryExecuter;
+use Firststep\Common\Builders\QueryBuilder;
+use Firststep\Common\Builders\MenuBuilder;
+use Firststep\Common\Database\DocumentDao;
 
 /**
  * 
  */
-class DocumentEdit extends Controller {
-	
-	function __construct($argument) {
-		# code...
+class DocumentEdit extends ManagerDocumentSenderController {
+
+    function __construct() {
+		$this->queryExecuter = new QueryExecuter;
+		$this->queryBuilder = new QueryBuilder;
+		$this->jsonBlockFormParser = new JsonBlockFormParser;
+		$this->menubuilder = new MenuBuilder;
+		$this->documentDao = new DocumentDao;
+    }
+
+    /**
+     * @throws GeneralException
+     */
+	public function getRequest() {
+		$this->queryExecuter->setDBH( $this->dbconnection->getDBH() );
+	    $this->queryExecuter->setQueryBuilder( $this->queryBuilder );
+	    $this->queryExecuter->setQueryStructure( $this->resource->query );
+	    $this->queryExecuter->setParameters( $this->internalGetParameters );
+
+		$formBlock = $this->jsonBlockFormParser->parse( 
+			$this->resource, 
+			null,
+			$this->router->make_url( Router::ROUTE_OFFICE_DOCUMENT_NEW, 'res='.$this->getParameters['res'] )
+		);
+		
+		$this->title = $this->setup->getAppNameForPageTitle() . ' :: Document new';
+
+		$menuresource = $this->jsonloader->loadResource( $this->sessionWrapper->getSessionGroup() );
+		$this->menubuilder->setMenuStructure( $menuresource );
+		$this->menubuilder->setRouter( $this->router );
+		
+		$this->menucontainer    = array( $this->menubuilder->createMenu() );
+		$this->leftcontainer    = array( new AdminSidebar( $this->setup->getAppNameForPageTitle(), Router::ROUTE_ADMIN_ENTITY_LIST, $this->router ) );
+		$this->centralcontainer = array( $formBlock );
 	}
+	
+	public function postRequest() {
+		// updating the document table
+		$this->documentDao->setDBH( $this->dbconnection->getDBH() );
+		$this->documentDao->setTableName( $this->resource->name );
+		
+		// removing save button from parameters and adding user id and user group
+		$queryparameters = $this->postParameters;
+		$queryparameters['sourceuserid'] = $this->sessionWrapper->getSessionUserId();
+		$queryparameters['sourcegroup'] = $this->sessionWrapper->getSessionGroup();
+		unset( $queryparameters['save'] );
+		
+		// saving in database
+		$this->documentDao->insert( $queryparameters );
+		
+		// applying the possible logics
+		$this->queryExecuter->setDBH( $this->dbconnection->getDBH() );
+
+		foreach ( $this->resource->logics->oninsert as $logic ) {
+			$this->queryExecuter->setQueryBuilder( $this->queryBuilder );
+	    	$this->queryExecuter->setQueryStructure( $logic );
+	    	$this->queryExecuter->setParameters( $this->postParameters );
+
+			$this->queryExecuter->executeQuery();
+		}
+
+		$this->redirectToSecondPreviousPage();
+	}
+
+    public function show_second_get_error_page() {
+        throw new ErrorPageException('Error page exception function show_get_error_page()');
+    }
 
 }
--- src/Controllers/Office/Document/DocumentInfo.php
+++ src/Controllers/Office/Document/DocumentInfo.php
@@ -2,15 +2,56 @@
 
 namespace Firststep\Controllers\Office\Document;
 
-use Firststep\Common\Controllers\Controller;
+use Firststep\Common\Controllers\ManagerEntityController;
+use Firststep\Templates\Blocks\Sidebars\AdminSidebar;
+use Firststep\Common\Json\JsonBlockParser;
+use Firststep\Common\Blocks\Button;
+use Firststep\Common\Router\Router;
+use Firststep\Common\Database\QueryExecuter;
+use Firststep\Common\Builders\QueryBuilder;
+use Firststep\Common\Builders\InfoBuilder;
+use Firststep\Common\Builders\MenuBuilder;
 
 /**
  * 
  */
-class DocumentInfo extends Controller {
+class DocumentInfo extends ManagerEntityController {
+
+    function __construct() {
+		$this->queryExecuter = new QueryExecuter;
+		$this->queryBuilder = new QueryBuilder;
+		$this->infoBuilder = new InfoBuilder;
+		$this->menubuilder = new MenuBuilder;
+    }
+
+    /**
+     * @throws GeneralException
+     */
+	public function getRequest() {
+		$menuresource = $this->jsonloader->loadResource( $this->sessionWrapper->getSessionGroup() );
+		$this->menubuilder->setMenuStructure( $menuresource );
+		$this->menubuilder->setRouter( $this->router );
+
+		$this->queryExecuter->setDBH( $this->dbconnection->getDBH() );
+	    $this->queryExecuter->setQueryBuilder( $this->queryBuilder );
+	    $this->queryExecuter->setQueryStructure( $this->resource->query );
+	    $this->queryExecuter->setParameters( $this->internalGetParameters );
+
+		$result = $this->queryExecuter->executeQuery();
+		$entity = $result->fetch();
+
+		$this->infoBuilder->setFormStructure( $this->resource->form );
+		$this->infoBuilder->setEntity( $entity );
+		
+		$this->title = $this->setup->getAppNameForPageTitle() . ' :: Info';
 	
-	function __construct($argument) {
-		# code...
+		$this->menucontainer    = array( $this->menubuilder->createMenu() );
+		$this->leftcontainer    = array( new AdminSidebar( $this->setup->getAppNameForPageTitle(), Router::ROUTE_ADMIN_ENTITY_LIST, $this->router ) );
+		$this->centralcontainer = array( $this->infoBuilder->createInfo() );
 	}
+
+    public function show_second_get_error_page() {
+        throw new ErrorPageException('Error page exception function show_get_error_page()');
+    }
 
 }
