7b413524eac6058ce48393c3843c563289b69401
Fabio Mattei <burattino@gmail.com>
2019-09-01 16:19:45 +0200

    adding form doc generator

--- src/Common/Json/DocBuilders/Form/FormV1DocBuilder.php
+++ src/Common/Json/DocBuilders/Form/FormV1DocBuilder.php
@@ -11,7 +11,15 @@ use Fabiom\UglyDuckling\Common\Utils\StringUtils;
 class FormV1DocBuilder extends BasicDocBuilder {
 
     public function getDocText() {
-        return $this->resource->name.'<br />';
+        $out = '\subsubsection{' . $this->resource->get->form->title . '}<br />';
+
+        $out .= $this->resource->get->query->sql . '<br />';
+
+        foreach ($this->resource->get->form->fields as $field) {
+            $out .= $field->headline . ' ' .  $field->sqlfield . '<br />';
+        }
+
+        return $out . '<br />';
     }
 
 }
