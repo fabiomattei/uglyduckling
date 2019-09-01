1ed702f6967f7df8fdfa5702f578b562d44c546d
Fabio Mattei <burattino@gmail.com>
2019-09-01 16:10:07 +0200

    generating chart doc

--- src/Common/Json/DocBuilders/Chartjs/ChartjsV1DocBuilder.php
+++ src/Common/Json/DocBuilders/Chartjs/ChartjsV1DocBuilder.php
@@ -11,7 +11,14 @@ use Fabiom\UglyDuckling\Common\Utils\StringUtils;
 class ChartjsV1DocBuilder extends BasicDocBuilder {
 
     public function getDocText() {
-        return $this->resource->name.'<br />';
+        $out = '\subsubsection{' . $this->resource->get->graphmeta->title . '}<br />';
+
+        $out .= $this->resource->get->query->sql . '<br />';
+
+        $out .= $this->resource->get->chart->options->scales->xAxes->scaleLabel->labelString . '<br />';
+        $out .= $this->resource->get->chart->options->scales->yAxes->scaleLabel->labelString . '<br />';
+
+        return $out . '<br />';
     }
 
 }
