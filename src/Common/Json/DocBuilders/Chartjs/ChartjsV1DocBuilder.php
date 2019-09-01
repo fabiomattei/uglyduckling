881978f0b7d81ad0cff57a6b81c36dc2eed9e186
Fabio Mattei <burattino@gmail.com>
2019-09-01 18:16:41 +0200

    adding latex to charts

--- src/Common/Json/DocBuilders/Chartjs/ChartjsV1DocBuilder.php
+++ src/Common/Json/DocBuilders/Chartjs/ChartjsV1DocBuilder.php
@@ -13,10 +13,22 @@ class ChartjsV1DocBuilder extends BasicDocBuilder {
     public function getDocText() {
         $out = '\subsubsection{' . $this->resource->get->graphmeta->title . '}<br />';
 
-        $out .= $this->resource->get->query->sql . '<br />';
+        $out .= '\begin{minted}{sql}' . '<br />';
+        $out .= wordwrap($this->resource->get->query->sql, 40, '<br />') . '<br />';
+        $out .= '\end{minted}' . '<br />';
 
-        $out .= 'X Axis: '.$this->resource->get->chart->options->scales->xAxes[0]->scaleLabel->labelString . ' ' . $this->resource->get->chartdataglue[1]->sqlfield . '<br />';
-        $out .= 'Y Axis: '.$this->resource->get->chart->options->scales->yAxes[0]->scaleLabel->labelString . ' ' . $this->resource->get->chartdataglue[0]->sqlfield . '<br />';
+        $out .= '\begin{table}[htbp]' . '<br />';
+        $out .= '\centering' . '<br />';
+        $out .= '\begin{tabular}{|l|l|l|}' . '<br />';
+        $out .= '\hline' . '<br />';
+
+        $out .= 'X Axis: & '.$this->resource->get->chart->options->scales->xAxes[0]->scaleLabel->labelString . ' & ' . $this->resource->get->chartdataglue[1]->sqlfield . '\\' . '<br />';
+        $out .= 'Y Axis: & '.$this->resource->get->chart->options->scales->yAxes[0]->scaleLabel->labelString . ' & ' . $this->resource->get->chartdataglue[0]->sqlfield . '\\' . '<br />';
+
+        $out .= '\hline' . '<br />';
+        $out .= '\end{tabular}' . '<br />';
+        $out .= '\end{table}' . '<br />';
+
 
         return $out . '<br />';
     }
--- src/Common/Json/DocBuilders/Table/TableV1DocBuilder.php
+++ src/Common/Json/DocBuilders/Table/TableV1DocBuilder.php
@@ -16,16 +16,14 @@ class TableV1DocBuilder extends BasicDocBuilder {
         $out .= '\begin{minted}{sql}' . '<br />';
         $out .= wordwrap($this->resource->get->query->sql, 40, '<br />') . '<br />';
         $out .= '\end{minted}' . '<br />';
-        
+
         $out .= '\begin{table}[htbp]' . '<br />';
         $out .= '\centering' . '<br />';
         $out .= '\begin{tabular}{|l|l|}' . '<br />';
         $out .= '\hline' . '<br />';
         $out .= 'Field name & SQL field \\' . '<br />';
         $out .= '\hline' . '<br />';
-
         foreach ($this->resource->get->table->fields as $field) {
-            $out .= 'Field name  & SQL field  \\';
             $out .= $field->headline . ' & ' .  $field->sqlfield . '\\' . '<br />';
         }
         $out .= '\hline' . '<br />';
