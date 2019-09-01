ce2c1b327394ae0d81b648aa27202923f4775fe5
Fabio Mattei <burattino@gmail.com>
2019-09-01 18:11:13 +0200

    adding latex info

--- src/Common/Json/DocBuilders/Table/TableV1DocBuilder.php
+++ src/Common/Json/DocBuilders/Table/TableV1DocBuilder.php
@@ -13,11 +13,24 @@ class TableV1DocBuilder extends BasicDocBuilder {
     public function getDocText() {
         $out = '\subsubsection{' . $this->resource->get->table->title . '}<br />';
 
+        $out .= '\begin{minted}{sql}' . '<br />';
         $out .= wordwrap($this->resource->get->query->sql, 40, '<br />') . '<br />';
+        $out .= '\end{minted}' . '<br />';
+        
+        $out .= '\begin{table}[htbp]' . '<br />';
+        $out .= '\centering' . '<br />';
+        $out .= '\begin{tabular}{|l|l|}' . '<br />';
+        $out .= '\hline' . '<br />';
+        $out .= 'Field name & SQL field \\' . '<br />';
+        $out .= '\hline' . '<br />';
 
         foreach ($this->resource->get->table->fields as $field) {
-           $out .= $field->headline . ' ' .  $field->sqlfield . '<br />';
+            $out .= 'Field name  & SQL field  \\';
+            $out .= $field->headline . ' & ' .  $field->sqlfield . '\\' . '<br />';
         }
+        $out .= '\hline' . '<br />';
+        $out .= '\end{tabular}' . '<br />';
+        $out .= '\end{table}' . '<br />';
 
         return $out . '<br />';
     }
