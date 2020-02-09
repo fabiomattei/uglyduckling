aca3ebbee5df1a2129b1c7e3ae025498ae45cd5b
Fabio Mattei <burattino@gmail.com>
2020-02-09 11:54:02 +0100

    adding logger to code

--- src/Common/Database/DBConnection.php
+++ src/Common/Database/DBConnection.php
@@ -3,11 +3,16 @@
 namespace Fabiom\UglyDuckling\Common\Database;
 
 use PDO;
+use PDOException;
+use Fabiom\UglyDuckling\Common\Loggers\Logger;
 
 /**
  * Basic class for all dao's
  */
 class DBConnection {
+
+    public PDO $DBH;
+    private Logger $logger;
 
     /**
      * Setting up the database connection
@@ -34,8 +39,7 @@ class DBConnection {
             $this->DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $this->DBH;
         } catch (PDOException $e) {
-            $logger = new Logger();
-            $logger->write($e->getMessage(), __FILE__, __LINE__);
+            $this->logger->write($e->getMessage(), __FILE__, __LINE__);
             throw new \Exception('General malfuction!!!');
         }   
     }
@@ -59,6 +63,16 @@ class DBConnection {
      */
     public function rollBack() {
         $this->DBH->rollBack();
+    }
+
+    /**
+     * Set the logger for the classe
+     *
+     * It is going to receive error messages in case of PDO exception
+     * @param Logger $logger
+     */
+    public function setLogger(Logger $logger) {
+        $this->logger = $logger;
     }
 
 }
