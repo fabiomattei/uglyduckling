39c8f340d60dab2a48a158f8f70ddb310c46c428
Fabio Mattei <burattino@gmail.com>
2020-02-28 23:05:21 +0100

    updating user form

--- src/Controllers/Admin/User/UserNew.php
+++ src/Controllers/Admin/User/UserNew.php
@@ -21,6 +21,8 @@ use Fabiom\UglyDuckling\Common\Router\Router;
 class UserNew extends Controller {
 
     private $userDao;
+    const FIELD_NEW_PASSWORD = 'usr_new_password';
+    const FIELD_RETYPE_NEW_PASSWORD = 'usr_retype_new_password';
 
     public function __construct() {
         $this->userDao = new UserDao;
@@ -40,8 +42,10 @@ class UserNew extends Controller {
         $form->setHtmlTemplateLoader( $this->htmlTemplateLoader );
         $form->setTitle( 'New user: ' );
         $form->addTextField('usr_email', 'Email: ', 'Email', '', '6' );
-        $form->addTextField('usr_name', 'Name: ', 'Name', $user->usr_name, '6' );
-        $form->addTextField('usr_surname', 'Surname: ', 'Surname', $user->usr_surname, '6' );
+        $form->addTextField('usr_name', 'Name: ', 'Name', '', '6' );
+        $form->addTextField('usr_surname', 'Surname: ', 'Surname', '', '6' );
+        $form->addPasswordField(UserEditPassword::FIELD_NEW_PASSWORD, 'New password:', '6' );
+        $form->addPasswordField(UserEditPassword::FIELD_RETYPE_NEW_PASSWORD, 'Retype new password:', '6' );
         $form->addSubmitButton('save', 'Save');
 
         $this->menucontainer    = array( new AdminMenu( $this->setup->getAppNameForPageTitle(), Router::ROUTE_ADMIN_USER_LIST ) );
@@ -54,12 +58,16 @@ class UserNew extends Controller {
     public $post_validation_rules = array(
         'usr_email' => 'required|valid_email',
         'usr_name' => 'max_len,100',
-        'usr_surname' => 'max_len,100'
+        'usr_surname' => 'max_len,100',
+        UserEditPassword::FIELD_NEW_PASSWORD => 'required|max_len,100|min_len,6',
+        UserEditPassword::FIELD_RETYPE_NEW_PASSWORD => 'required|max_len,100|min_len,6',
     );
     public $post_filter_rules     = array(
         'usr_email' => 'trim|sanitize_email',
         'usr_name' => 'trim',
-        'usr_surname' => 'trim'
+        'usr_surname' => 'trim',
+        UserEditPassword::FIELD_NEW_PASSWORD => 'trim',
+        UserEditPassword::FIELD_RETYPE_NEW_PASSWORD => 'trim'
     );
 
     /**
@@ -69,14 +77,20 @@ class UserNew extends Controller {
      */
     public function postRequest() {
         $this->userDao->setDBH( $this->dbconnection->getDBH() );
-        $this->userDao->insert( array(
-                'usr_name' => $this->postParameters['usr_name'],
-                'usr_surname' => $this->postParameters['usr_surname'],
-                'usr_email' => $this->postParameters['usr_email'],
-                'usr_password_updated' => date('Y-m-d')
-            )
-        );
-
+        if ( $this->parameters[UserEditPassword::FIELD_NEW_PASSWORD] == $this->parameters[UserEditPassword::FIELD_RETYPE_NEW_PASSWORD] ) {
+                $iduser = $this->userDao->insert( array(
+                    'usr_name' => $this->postParameters['usr_name'],
+                    'usr_surname' => $this->postParameters['usr_surname'],
+                    'usr_email' => $this->postParameters['usr_email'],
+                    'usr_password_updated' => date('Y-m-d'),
+                );
+                $this->userDao->updatePassword( $iduser, $this->postParameters[UserEditPassword::FIELD_NEW_PASSWORD]);
+                $this->setSuccess("Password successfully updated");
+                $this->redirectToSecondPreviousPage();
+            } else {
+                $this->setError("The two new password do not match");
+                $this->redirectToPreviousPage();
+            }
         $this->redirectToSecondPreviousPage();
     }
 
@@ -93,6 +107,8 @@ class UserNew extends Controller {
         $form->addTextField('usr_email', 'Email: ', 'Email', '', '6' );
         $form->addTextField('usr_name', 'Name: ', 'Name', '', '6' );
         $form->addTextField('usr_surname', 'Surname: ', 'Surname', '', '6' );
+        $form->addPasswordField(UserEditPassword::FIELD_NEW_PASSWORD, 'New password:', '6' );
+        $form->addPasswordField(UserEditPassword::FIELD_RETYPE_NEW_PASSWORD, 'Retype new password:', '6' );
         $form->addSubmitButton('save', 'Save');
 
         $this->menucontainer    = array( new AdminMenu( $this->setup->getAppNameForPageTitle(), Router::ROUTE_ADMIN_USER_LIST ) );
