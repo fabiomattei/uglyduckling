880d579e2d972b7e3207d03288d78ba0e5b6b0b3
Fabio Mattei <burattino@gmail.com>
2020-03-14 19:43:04 +0100

    correcting logger

--- src/Common/Loggers/EchoLogger.php
+++ src/Common/Loggers/EchoLogger.php
@@ -12,8 +12,8 @@ class EchoLogger implements Logger {
 
     public function write($message, $file='', $line='') {
         $message = date("Y-m-d H:i:s") .' - '.$message;
-        $message .= $file=='' ? '' : " in $file";
-        $message .= $line=='' ? '' : " on line $line";
+        $message .= ($file=='' ? '' : " in $file");
+        $message .= ($line=='' ? '' : " on line $line");
         $message .= "\n";
         echo '<b>Logger:</b> '.$message;
     }
