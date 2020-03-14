75b00eca9ce37835a40ee94e09b5ae30efe95cae
Fabio Mattei <burattino@gmail.com>
2020-03-14 19:36:20 +0100

    adding class MuteLogger

---
+++ src/Common/Loggers/MuteLogger.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Fabiom\UglyDuckling\Common\Loggers;
+
+/**
+ * Created fabio
+ * Date: 14/03/2020
+ * Time: 19:34
+ *
+ * This logger is a dev/null logger, all messages sent here are lost
+ */
+
+class MuteLogger implements Logger {
+
+    public function write($message, $file='', $line='') {
+        // nothing to do every message sent here is lost
+    }
+
+}
