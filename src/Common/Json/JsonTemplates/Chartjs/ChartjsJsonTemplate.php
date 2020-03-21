c11e81ffd503614a7777d26f6cc7590afaffd41d
Fabio Mattei <burattino@gmail.com>
2020-03-21 21:21:49 +0100

    updating content

--- src/Common/Json/JsonTemplates/Chartjs/ChartjsJsonTemplate.php
+++ src/Common/Json/JsonTemplates/Chartjs/ChartjsJsonTemplate.php
@@ -1,16 +1,15 @@
 <?php
 
-/**
- * Created by Fabio Mattei
- * Date: 01/11/18
- * Time: 10.15
- */
-
 namespace Fabiom\UglyDuckling\Common\Json\JsonTemplates\Chartjs;
 
 use Fabiom\UglyDuckling\Common\Blocks\BaseHTMLChart;
 use Fabiom\UglyDuckling\Common\Json\JsonTemplates\JsonTemplate;
 
+/**
+ * Created by Fabio Mattei
+ * Date: 01/11/18
+ * Time: 10.15
+ */
 class ChartjsJsonTemplate extends JsonTemplate {
 
     const blocktype = 'chartjs';
