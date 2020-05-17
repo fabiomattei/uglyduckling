9223fd01b6d9f750b03ed82d51ab4eb032822634
Fabio Mattei <burattino@gmail.com>
2020-05-17 15:45:11 +0200

    simplifying code

--- src/Common/Tags/HTMLTagsFactory.php
+++ src/Common/Tags/HTMLTagsFactory.php
@@ -10,8 +10,6 @@ use Fabiom\UglyDuckling\Common\Tags\DefaultTags\HTMLLinkTag;
 class HTMLTagsFactory {
 
     protected /* array */ $htmlTags;
-    private /* TableJsonTemplate */ $htmlButtonTag;
-    private /* ChartjsJsonTemplate */ $htmlLinkTag;
 
     function __construct() {
         $this->htmlTags = array();
@@ -22,26 +20,14 @@ class HTMLTagsFactory {
      * This function load the standard HTML tags defined by UD
      */
     function loadDefaults() {
-        $this->htmlButtonTag = new HTMLButtonTag;
-        $this->htmlLinkTag = new HTMLLinkTag;
-        $this->addHTMLTag($this->htmlButtonTag);
-        $this->addHTMLTag($this->htmlLinkTag);
+        $this->htmlButtonTag[HTMLButtonTag::BLOCK_TYPE] = new HTMLButtonTag;
+        $this->htmlLinkTag[HTMLLinkTag::BLOCK_TYPE] = new HTMLLinkTag;
     }
 
     /**
-     * This method is ment to be overloaded in order to load custom HTML tags
+     * This method is ment to be everrided in order to load custom HTML tags
      */
     function loadCustom() {}
-
-    /**
-     * Add an HTMLTag to the factory
-     * @param BaseHTMLTag $htmlTag
-     */
-    public function addHTMLTag( BaseHTMLTag $htmlTag ) {
-        if (property_exists(get_class ($htmlTag), 'BLOCK_TYPE')) {
-            $this->htmlTags[$htmlTag::BLOCK_TYPE] = $htmlTag;
-        }
-    }
 
     /**
      * @param $jsonStructure
