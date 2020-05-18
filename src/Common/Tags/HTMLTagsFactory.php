6f95b6256e175b37ce6af95d759f0fd29a200c22
Fabio Mattei <burattino@gmail.com>
2020-05-18 11:01:17 +0200

    fixing problem

--- src/Common/Tags/HTMLTagsFactory.php
+++ src/Common/Tags/HTMLTagsFactory.php
@@ -12,16 +12,8 @@ class HTMLTagsFactory {
     protected /* array */ $htmlTags = array();
 
     function __construct() {
-        $this->htmlTags[HTMLButtonTag::BLOCK_TYPE] = new HTMLButtonTag;
-        $this->htmlTags[HTMLLinkTag::BLOCK_TYPE] = new HTMLLinkTag;
-    }
-
-    /**
-     * This function load the standard HTML tags defined by UD
-     */
-    function loadDefaults() {
-        $this->htmlButtonTag[HTMLButtonTag::BLOCK_TYPE] = new HTMLButtonTag;
-        $this->htmlLinkTag[HTMLLinkTag::BLOCK_TYPE] = new HTMLLinkTag;
+        $this->htmlTags[HTMLButtonTag::BLOCK_TYPE] = 'HTMLButtonTag';
+        $this->htmlTags[HTMLLinkTag::BLOCK_TYPE] = 'HTMLLinkTag';
     }
 
     /**
@@ -32,7 +24,7 @@ class HTMLTagsFactory {
      */
     public function getHTMLTag( $jsonStructure, PageStatus $pageStatus, ApplicationBuilder $applicationBuilder ): BaseHTMLTag {
         if ( key_exists($jsonStructure->type , $this->htmlTags) ) {
-            $htmlTag = $this->htmlTags[$jsonStructure->type];
+            $htmlTag = new $this->htmlTags[$jsonStructure->type];
             $htmlTag->setResources($jsonStructure, $pageStatus, $applicationBuilder);
             return $htmlTag;
         } else {
