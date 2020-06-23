9995e44677779877cc89778b3359821a99c98d01
Fabio Mattei <burattino@gmail.com>
2020-06-23 06:32:37 +0200

    adding null mailer

---
+++ src/Common/Mailer/NullMailer.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created Fabio Mattei
+ * Date: 23-06-2020
+ * Time: 06:32
+ */
+
+namespace Fabiom\UglyDuckling\Common\Mailer;
+
+/**
+ * Class EchoMailer
+ *
+ * This class gives the structure to create a Mail.
+ *
+ */
+class NullMailer {
+
+	function send( string $dest_email, string $from_email, string $subject, string $message ) { 
+		// null
+	}
+
+}
