f4f8990ac3630a621daae13c9152fa87065394d6
Fabio Mattei <burattino@gmail.com>
2020-09-19 14:45:23 +0200

    activating buttons and links

--- src/Common/Tags/DefaultTags/HTMLLinkTag.php
+++ src/Common/Tags/DefaultTags/HTMLLinkTag.php
@@ -23,7 +23,8 @@ class HTMLLinkTag extends BaseHTMLTag {
      * Takes a JSON resource (object or array or composite) and convert it in HTML
      */
     function getHTML(): string {
-        return '';
+        $url = $this->applicationBuilder->make_resource_url( $this->jsonStructure, $this->applicationBuilder->getJsonloader(), $this->pageStatus );
+        return '<a href="'.$url.'">'.$this->jsonStructure->label.'</a>';
     }
 
 }
