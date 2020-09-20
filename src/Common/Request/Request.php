2e628a88a42866b583a0844e6e73333170d34c8e
Fabio Mattei <burattino@gmail.com>
2020-09-20 05:52:55 +0200

    cleaning more codee

--- src/Common/Request/Request.php
+++ src/Common/Request/Request.php
@@ -14,38 +14,10 @@ class Request {
 
     public function setServerRequestURI( string $requestURI ) {
         $this->requestURI = $requestURI;
-        $this->calculateSplittedURL();
     }
 
     public function getAction() {
         return $this->action;
-    }
-
-    /**
-    * @param $request  a string containing the request
-    *
-    * @return array    an array containing the results
-    *
-    * @throws \Exception    in case of empty request
-    *
-    * Prende una stringa e la divide nelle sue parti.
-    * Restituisce poi le parti ottenute attraverso un array.
-    *
-    * Es. 'folder-subfolder/action/par1/par2/par3'
-    * Diventa array( 'action', array( 'par1', 'par2', 'par3' ) )
-    */
-    public function calculateSplittedURL() {
-        $request2 = str_replace( '.html', '', $this->requestURI );
-        $request3 = str_replace( '.pdf', '', $request2 );
-        $request  = preg_replace( '/\?.*/', '', $request3 );
-
-        if ( $request == '' ) throw new \Exception('General malfuction!!!');
-
-        # removing the first '/' from the path
-        $this->action = substr($request, 1); // explode( '/', $request );
-
-        if (!StringUtils::validate_string( $this->action ))
-            throw new \Exception('Illegal access to calculateSplittedURL!!!');
     }
 
     public function getInfo(): string {
