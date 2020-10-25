ad3218979596c2c3b04771770032bfac9927cdf9
Fabio Mattei <burattino@gmail.com>
2020-10-25 17:57:17 +0100

    figin database linking bug

--- src/Common/Controllers/AdminController.php
+++ src/Common/Controllers/AdminController.php
@@ -20,6 +20,7 @@ class AdminController extends Controller {
      */
     public function showPage() {
         $groupDao = new UserGroupDao;
+        $groupDao->setDBH( $this->pageStatus->getDbconnection()->getDBH() );
         if (
             $this->pageStatus->getSessionWrapper()->getSessionGroup() != 'administrationgroup' OR
             $groupDao->checkUserHasAccessToGroup( $this->pageStatus->getSessionWrapper()->getSessionUserId(), 'administrationgroup' )
