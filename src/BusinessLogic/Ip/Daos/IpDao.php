2df6a5c52431160f495179dbccd4b2c1b8c73663
Fabio Mattei <burattino@gmail.com>
2020-10-25 10:28:28 +0100

    fixing query

--- src/BusinessLogic/Ip/Daos/IpDao.php
+++ src/BusinessLogic/Ip/Daos/IpDao.php
@@ -42,7 +42,7 @@ class IpDao extends BasicDao {
 	function checkIfIpIsBlocker( $remote_address ) {
 		try {
 			
-			$STH = $this->DBH->prepare('SELECT ip_ipaddress FROM blockedip WHERE ip_ipaddress = :ipaddress AND ip_time_to_remove < NOW();');
+			$STH = $this->DBH->prepare('SELECT ip_ipaddress FROM blockedip WHERE ip_ipaddress = :ipaddress AND NOW() < ip_time_to_remove;');
 			$STH->bindParam(':ipaddress', $remote_address, PDO::PARAM_STR);
 			$STH->execute();
 			
