4be02436eb2c3888cb527c90b4ad47c201f49ecd
Fabio Mattei <burattino@gmail.com>
2020-10-26 14:16:52 +0100

    addin ip is not blocked

--- src/BusinessLogic/Ip/UseCases/IpIsNotBlocked.php
+++ src/BusinessLogic/Ip/UseCases/IpIsNotBlocked.php
@@ -5,7 +5,14 @@ namespace Fabiom\UglyDuckling\BusinessLogic\Ip\UseCases;
 class IpIsNotBlocked {
 	
 	public function performAction( $remote_address, $ipDao ) {
+		$ips = array('80.211.55.253','212.237.42.12','212.237.42.215','212.237.42.207','185.41.215.50','217.61.26.74','217.61.26.75','217.61.26.79','217.61.26.82','217.61.26.84');
+		if (in_array( $remote_address, $ips )) {
+		    return true;
+		}
 		return !$ipDao->checkIfIpIsBlocker( $remote_address );
 	}
 	
 }
+
+
+
