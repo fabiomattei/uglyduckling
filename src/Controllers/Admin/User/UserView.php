02c8e4cd2696494068f13dcfcf3b690cecdcc4eb
Fabio Mattei <burattino@gmail.com>
2020-11-15 11:03:21 +0100

    making interface more strong

--- src/Controllers/Admin/User/UserView.php
+++ src/Controllers/Admin/User/UserView.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by Fabio Mattei
- * 
+ *
  * Date: 21/10/2018
  * Time: 10:40
  */
@@ -20,11 +20,12 @@ use Fabiom\UglyDuckling\Common\Blocks\ButtonForm;
 /**
  * This class gives a list of all entities loaded in to the system
  */
-class UserView extends AdminController {
-
+class UserView extends AdminController
+{
     private $userDao;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->userDao = new UserDao;
     }
 
@@ -36,33 +37,32 @@ class UserView extends AdminController {
      *
      * $this->getParameters['res'] resource key index
      */
-    public function getRequest() {
-        $this->userDao->setDBH( $this->pageStatus->getDbconnection()->getDBH() );
-        $user = $this->userDao->getById( $this->getParameters['id'] );
+    public function getRequest()
+    {
+        $this->userDao->setDBH($this->pageStatus->getDbconnection()->getDBH());
+        $user = $this->userDao->getById($this->getParameters['id']);
 
         $this->title = $this->applicationBuilder->getSetup()->getAppNameForPageTitle() . ' :: User view';
 
         $info = new BaseHTMLInfo;
-        $info->setHtmlTemplateLoader( $this->applicationBuilder->getHtmlTemplateLoader() );
-        $info->setTitle( 'User: ' . $user->usr_name . ' ' . $user->usr_surname );
+        $info->setHtmlTemplateLoader($this->applicationBuilder->getHtmlTemplateLoader());
+        $info->setTitle('User: ' . $user->usr_name . ' ' . $user->usr_surname);
         $info->addUnfilteredParagraph(
-            Button::get($this->applicationBuilder->getRouterContainer()->makeRelativeUrl( AdminRouter::ROUTE_ADMIN_USER_EDIT, 'id='.$user->usr_id ), 'Edit', Button::COLOR_GRAY.' '.Button::SMALL ) . ' ' .
-            Button::get($this->applicationBuilder->getRouterContainer()->makeRelativeUrl( AdminRouter::ROUTE_ADMIN_USER_EDIT_PASSWORD, 'id='.$user->usr_id ), 'Edit password', Button::COLOR_GRAY.' '.Button::SMALL ) . ' ' .
-            ButtonForm::get($this->applicationBuilder->getRouterContainer()->makeRelativeUrl( AdminRouter::ROUTE_ADMIN_USER_DELETE ), 'Del', $this->pageStatus->getSessionWrapper()->getCsrfToken(), array('usrid' => $user->usr_id), Button::COLOR_RED.' '.Button::SMALL )
-            ,
+            Button::get($this->applicationBuilder->getRouterContainer()->makeRelativeUrl(AdminRouter::ROUTE_ADMIN_USER_EDIT, 'id='.$user->usr_id), 'Edit', Button::COLOR_GRAY.' '.Button::SMALL) . ' ' .
+            Button::get($this->applicationBuilder->getRouterContainer()->makeRelativeUrl(AdminRouter::ROUTE_ADMIN_USER_EDIT_PASSWORD, 'id='.$user->usr_id), 'Edit password', Button::COLOR_GRAY.' '.Button::SMALL) . ' ' .
+            ButtonForm::get($this->applicationBuilder->getRouterContainer()->makeRelativeUrl(AdminRouter::ROUTE_ADMIN_USER_DELETE), 'Del', $this->pageStatus->getSessionWrapper()->getCsrfToken(), array('usrid' => $user->usr_id), Button::COLOR_RED.' '.Button::SMALL),
             '6'
         );
-        $info->addTextField('Default group: ', $user->usr_defaultgroup, '6' );
-        $info->addTextField('Email: ', $user->usr_email, '6' );
-        $info->addDateField('Password updated: ', $user->usr_password_updated 	, '6' );
-        $info->addDateField('User created: ', $user->usr_updated , '6' );
-        $info->addDateField('User updated: ', $user->usr_created, '6' );
+        $info->addTextField('Default group: ', $user->usr_defaultgroup, '6');
+        $info->addTextField('Email: ', $user->usr_email ?? '', '6');
+        $info->addDateField('Password updated: ', $user->usr_password_updated ?? '', '6');
+        $info->addDateField('User created: ', $user->usr_updated ?? '', '6');
+        $info->addDateField('User updated: ', $user->usr_created ?? '', '6');
 
-        $this->menucontainer    = array( new AdminMenu( $this->applicationBuilder->getSetup()->getAppNameForPageTitle(), AdminRouter::ROUTE_ADMIN_USER_LIST ) );
-        $this->leftcontainer    = array( new AdminSidebar( $this->applicationBuilder->getSetup()->getAppNameForPageTitle(), AdminRouter::ROUTE_ADMIN_USER_LIST, $this->applicationBuilder->getRouterContainer() ) );
+        $this->menucontainer    = array( new AdminMenu($this->applicationBuilder->getSetup()->getAppNameForPageTitle(), AdminRouter::ROUTE_ADMIN_USER_LIST) );
+        $this->leftcontainer    = array( new AdminSidebar($this->applicationBuilder->getSetup()->getAppNameForPageTitle(), AdminRouter::ROUTE_ADMIN_USER_LIST, $this->applicationBuilder->getRouterContainer()) );
         $this->centralcontainer = array( $info );
 
         $this->templateFile = $this->applicationBuilder->getSetup()->getPrivateTemplateWithSidebarFileName();
     }
-
 }
