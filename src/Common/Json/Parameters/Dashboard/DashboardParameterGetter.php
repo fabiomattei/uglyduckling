a1165bfe90fbd7e03dfdfbd59ea6826b8d0fd4db
Fabio Mattei <burattino@gmail.com>
2022-03-23 19:02:03 +0100

    fixing problem with automatic check

--- src/Common/Json/Parameters/Dashboard/DashboardParameterGetter.php
+++ src/Common/Json/Parameters/Dashboard/DashboardParameterGetter.php
@@ -15,9 +15,11 @@ class DashboardParameterGetter extends BasicParameterGetter {
     function getGetParameters(): array {
         $parameters = array();
         foreach( $this->resource->panels as $panel ) {
-            $json_resource = $this->jsonloader->loadResource( $panel->resource );
-            $parGetter = BasicParameterGetter::basicParameterCheckerFactory( $json_resource, $this->jsonloader );
-            $parameters = array_merge( $parameters, $parGetter->getGetParameters() );
+            if ($this->applicationBuilder->getJsonloader()->isJsonResourceIndexedAndFileExists( $panel->resource )) {
+                $json_resource = $this->jsonloader->loadResource( $panel->resource );
+                $parGetter = BasicParameterGetter::basicParameterCheckerFactory( $json_resource, $this->jsonloader );
+                $parameters = array_merge( $parameters, $parGetter->getGetParameters() );
+            }
         }
         return $parameters;
     }
