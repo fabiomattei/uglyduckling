ae2c3128bcfca1563b5840b52c1bdea1605acdd2
Fabio Mattei <burattino@gmail.com>
2022-04-14 21:15:41 +0200

    creating a controller as a base to bypass csrftoken

---
+++ src/Common/Controllers/ControllerNoCSRFTokenRenew.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Fabiom\UglyDuckling\Common\Controllers;
+
+class ControllerNoCSRFTokenRenew extends Controller {
+
+    public function showPage() {
+        $time_start = microtime(true);
+
+        if ($this->pageStatus->getServerWrapper()->isGetRequest()) {
+            if ( $this->check_authorization_get_request() ) {
+                if ( $this->check_get_request() ) {
+                    $this->getRequest();
+                } else {
+                    $this->show_get_error_page();
+                }
+            } else {
+                $this->check_authorization_get_request();
+            }
+        } else {
+            if ( $this->check_authorization_post_request() ) {
+                if ( $this->check_post_request() ) {
+                    $this->postRequest();
+                } else {
+                    $this->show_post_error_page();
+                }
+            } else {
+                $this->check_authorization_post_request();
+            }
+        }
+
+        $this->loadTemplate();
+
+        $time_end = microtime(true);
+        if (($time_end - $time_start) > 5) {
+            $this->applicationBuilder->getLogger()->write('WARNING TIME :: ' . $this->request->getInfo() . ' - TIME: ' . ($time_end - $time_start) . ' sec', __FILE__, __LINE__);
+        }
+    }
+
+}
