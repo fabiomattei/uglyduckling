e8d338078125be4db040a425234cdebe190ec3c2
Fabio Mattei <burattino@gmail.com>
2022-07-06 21:30:41 +0200

    implementing ajax logic

--- src/Common/Controllers/JsonResourcePartialBasicController.php
+++ src/Common/Controllers/JsonResourcePartialBasicController.php
@@ -2,9 +2,8 @@
 
 namespace Fabiom\UglyDuckling\Common\Controllers;
 
-use Fabiom\UglyDuckling\Common\Database\QueryExecuter;
-use Fabiom\UglyDuckling\Common\Database\QueryReturnedValues;
 use Fabiom\UglyDuckling\Common\Json\Parameters\BasicParameterGetter;
+use Fabiom\UglyDuckling\Common\Status\Logics;
 
 class JsonResourcePartialBasicController extends ControllerNoCSRFTokenRenew {
 
@@ -22,9 +21,12 @@ class JsonResourcePartialBasicController extends ControllerNoCSRFTokenRenew {
         // loading json resource
         if ( ! $jsonResourceName ) {
             echo 'missing resource name';
+            $jsonResource = new \stdClass;
         } else {
             if ( strlen( $jsonResourceName ) > 0 ) {
                 $jsonResource = $this->applicationBuilder->getJsonloader()->loadResource( $jsonResourceName );
+            } else {
+                $jsonResource = new \stdClass;
             }
         }
 
@@ -76,59 +78,32 @@ class JsonResourcePartialBasicController extends ControllerNoCSRFTokenRenew {
     public function postRequest() {
         $this->templateFile = "empty";
 
-        $this->queryExecutor = $this->pageStatus->getQueryExecutor();
-        $this->queryExecutor->setApplicationBuilder( $this->applicationBuilder );
+        $this->applicationBuilder->getJsonloader()->loadIndex();
 
-        $conn = $this->pageStatus->getDbconnection()->getDBH();
+        // GETTING json resource name from parameter
+        $jsonResourceName = filter_input(INPUT_POST | INPUT_GET, 'res', FILTER_SANITIZE_STRING);
 
-        // performing transactions
-        if (isset($this->resource->post->transactions)) {
-            $returnedIds = new QueryReturnedValues;
-            try {
-                //$conn->beginTransaction();
-                $this->queryExecutor->setDBH( $conn );
-                foreach ($this->resource->post->transactions as $transaction) {
-                    $this->queryExecutor->setQueryStructure( $transaction );
-                    if ( $this->queryExecutor->getSqlStatmentType() == QueryExecuter::INSERT) {
-                        if (isset($transaction->label)) {
-                            $returnedIds->setValue($transaction->label, $this->queryExecutor->executeSql());
-                        } else {
-                            $returnedIds->setValueNoKey($this->queryExecutor->executeSql());
-                        }
-                    } else {
-                        $this->queryExecutor->executeSql();
-                    }
-                }
-                //$conn->commit();
-            }
-            catch (\PDOException $e) {
-                $conn->rollBack();
-                $this->applicationBuilder->getLogger()->write($e->getMessage(), __FILE__, __LINE__);
+        // loading json resource
+        if ( ! $jsonResourceName ) {
+            echo 'missing resource name';
+            $jsonResource = new \stdClass;
+        } else {
+            if ( strlen( $jsonResourceName ) > 0 ) {
+                $jsonResource = $this->applicationBuilder->getJsonloader()->loadResource( $jsonResourceName );
+            } else {
+                $jsonResource = new \stdClass;
             }
         }
 
-        // performing usecases
-        if (isset($this->resource->post->usecases) and is_array($this->resource->post->usecases)) {
-            foreach ($this->resource->post->usecases as $jsonusecase) {
-                $useCase = $this->pageStatus->getUseCasesIndex()->getUseCase($jsonusecase, $this->pageStatus, $this->applicationBuilder);
-                $useCase->performAction();
-            }
-        }
+        Logics::performTransactions( $this->pageStatus, $this->applicationBuilder, $jsonResource );
 
-        // if resource->post->sessionupdates is set I need to update the session
-        if ( isset($this->resource->post->sessionupdates) ) $this->pageStatus->updateSession( $this->resource->post->sessionupdates );
+        Logics::performUseCases( $this->pageStatus, $this->applicationBuilder, $jsonResource );
 
-        // redirect
-        if (isset($this->resource->post->redirect)) {
-            $this->jsonRedirector($this->resource->post->redirect);
-        }
-        if ( !isset($this->resource->post->redirect) AND !isset($this->resource->post->render) ) {
-            // I need this to replicate previous default redirect
-            $this->redirectToPreviousPage();
-        }
+        // if resource->get->sessionupdates is set I need to update the session
+        if ( isset($this->resource->get->sessionupdates) ) $this->pageStatus->updateSession( $this->resource->get->sessionupdates );
 
-        if (isset($this->resource->post->render)) {
-            echo $this->applicationBuilder->getBlock( $this->resource->post->render->resource )->show();
+        if ( isset($jsonResource->post->ajax) ) {
+            echo Logics::performAjaxCallPost( $this->pageStatus, $this->applicationBuilder, $jsonResource );
         }
     }
 
