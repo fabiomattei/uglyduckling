74118c5fc3bfe4d59c4f512b54fb190afd1d0d0f
Fabio Mattei <burattino@gmail.com>
2023-04-09 10:16:00 +0200

    working on administration docs export

--- src/Controllers/Admin/DocsExport/AdminDocsExport.php
+++ src/Controllers/Admin/DocsExport/AdminDocsExport.php
@@ -4,6 +4,7 @@ namespace Fabiom\UglyDuckling\Controllers\Admin\DocsExport;
 
 use Fabiom\UglyDuckling\Common\Blocks\Button;
 use Fabiom\UglyDuckling\Common\Blocks\StaticTable;
+use Fabiom\UglyDuckling\Common\Blocks\BaseHTMLInfo;
 use Fabiom\UglyDuckling\Common\Controllers\AdminController;
 use Fabiom\UglyDuckling\Common\Router\AdminRouter;
 use Fabiom\UglyDuckling\Templates\Blocks\Menus\AdminMenu;
@@ -17,34 +18,56 @@ class AdminDocsExport extends AdminController {
         $this->title = $this->applicationBuilder->getSetup()->getAppNameForPageTitle() . ' :: Admin Docs export';
 
         $docsList = [];
-        foreach ( $this->applicationBuilder->getJsonloader()->getResourcesByType( 'group' ) as $res ) {
+        foreach ( $this->applicationBuilder->getJsonloader()->getResourcesByType( 'group' ) as $jsonGroupName ) {
+            $jsonGroup = $this->applicationBuilder->getJsonloader()->loadResource($jsonGroupName->name);
+
+            $info = new BaseHTMLInfo;
+            $info->setHtmlTemplateLoader( $this->applicationBuilder->getHtmlTemplateLoader() );
+            $info->setTitle('Group: ' . $jsonGroup->name);
+            if ( isset($jsonGroup->docs) and is_array($jsonGroup->docs) ) {
+                foreach ( $jsonGroup->docs as $paragraph) {
+                    $info->addParagraph($paragraph, 12);
+                }
+            }
+
+            $docsList[] = $info;
+
             $table = new StaticTable;
             $table->setHtmlTemplateLoader( $this->applicationBuilder->getHtmlTemplateLoader() );
-            $table->setTitle('Docs');
+            $table->setTitle('Menu structure');
             $table->addTHead();
             $table->addRow();
-            $table->addHeadLineColumn('Name');
-            $table->addHeadLineColumn('Type');
-            $table->addHeadLineColumn(''); // adding one more for actions
+            $table->addHeadLineColumn('Menu');
+            $table->addHeadLineColumn('Sub-menu');
             $table->closeRow();
             $table->closeTHead();
 
             $table->addTBody();
-            $table->addRow();
-            $table->addColumn($res->name);
-            $table->addColumn($res->type);
-            $table->addUnfilteredColumn(
-                Button::get($this->applicationBuilder->getRouterContainer()->makeRelativeUrl( AdminRouter::ROUTE_ADMIN_GROUP_VIEW, 'res='.$res->name ), 'View', Button::COLOR_GRAY.' '.Button::SMALL ) . ' ' .
-                Button::get($this->applicationBuilder->getRouterContainer()->makeRelativeUrl( AdminRouter::ROUTE_ADMIN_GROUP_DOC, 'res='.$res->name ), 'Doc', Button::COLOR_GRAY.' '.Button::SMALL )
-            );
-            $table->closeRow();
+
+            if ( isset($jsonGroup->menu) and is_array($jsonGroup->menu) ) {
+                foreach ( $jsonGroup->menu as $menu) {
+                    $table->addRow();
+                    $table->addColumn( $menu->label );
+                    $table->addColumn( '');
+                    $table->closeRow();
+                    if ( isset($menu->submenu) and is_array($menu->submenu) ) {
+                        foreach ( $menu->submenu as $submenuitem) {
+                            $table->addRow();
+                            $table->addColumn( '');
+                            $table->addColumn( $submenuitem->label );
+                            $table->closeRow();
+                        }
+                    }
+                }
+            }
+
             $table->closeTBody();
             $docsList[] = $table;
         }
 
         $this->menucontainer    = array( new AdminMenu( $this->applicationBuilder->getSetup()->getAppNameForPageTitle(), AdminRouter::ROUTE_ADMIN_GROUP_LIST ) );
         $this->leftcontainer    = array( new AdminSidebar( $this->applicationBuilder->getSetup()->getAppNameForPageTitle(), AdminRouter::ROUTE_ADMIN_GROUP_LIST, $this->applicationBuilder->getRouterContainer() ) );
-        $this->centralcontainer = array( $docsList );
+        $this->centralcontainer = $docsList;
 
         $this->templateFile = $this->applicationBuilder->getSetup()->getPrivateTemplateWithSidebarFileName();
     }
