87f0ec9119a6d23869f4f6e5c61e0e40bab1c0a4
Fabio Mattei <burattino@gmail.com>
2023-12-10 05:30:25 +0100

    cleaning

--- src/Framework/Json/templates.php
+++ src/Framework/Json/templates.php
@@ -5,66 +5,6 @@ use Fabiom\UglyDuckling\Common\Blocks\CardHTMLBlock;
 
 $GLOBALS['myTemplateFunctions'] = [];
 
-$GLOBALS['myTemplateFunctions']['dashboard'] = function ( $jsonResource, $jsonLoader, $breadcrumb, $level ) {
-    $doc = new BaseHtmlDoc;
-    $docs = [$doc];
-    if ( isset($jsonResource->description) and is_string($jsonResource->description) ) {
-        $doc->paragraph($jsonResource->description);
-    }
-    if ( isset($jsonResource->docs) and is_array($jsonResource->docs) ) {
-        foreach ( $jsonResource->docs as $paragraph) {
-            $doc->paragraph($paragraph);
-        }
-    }
-    if (count($jsonResource->panels) > 1) {
-        // $doc->paragraph('This page is composed by '. count($jsonResource->panels) . ' sections ');
-    }
-    if (isset($jsonResource->docimg) and is_string($jsonResource->docimg) ) {
-        $doc->img($jsonResource->docimg, ['width'=>'700']);
-    }
-    if (isset($jsonResource->docimgs) and is_array($jsonResource->docimgs) ) {
-        foreach ( $jsonResource->docimgs as $imgs) {
-            $doc->img($imgs, ['width'=>'700']);
-        }
-    }
-    if ( !isset($jsonResource->nopanels) ) {
-        $chapterNumber = 1;
-        foreach ($jsonResource->panels as $panel) {
-            if ( $jsonLoader->isJsonResourceIndexedAndFileExists($panel->resource) ) {
-                $panelResource = $jsonLoader->loadResource($panel->resource);
-                // echo( gettype($panelResource->metadata->type));
-                // echo($panelResource->metadata->type."<br>");
-                $items = $GLOBALS['myDocFunctions'][$panelResource->metadata->type]($panelResource, $jsonLoader, $breadcrumb . '.' . $chapterNumber, $level + 1);
-                if (is_array($items)) {
-                    $docs = array_merge($docs, $items);
-                } else {
-                    $docs[] = $items;
-                }
-            } else {
-                echo $panel->resource." does not exist<br>";
-            }
-            $chapterNumber += 1;
-        }
-    }
-    if (isset($jsonResource->docprocess) and is_array($jsonResource->docprocess)) {
-        foreach ($jsonResource->docprocess as $processResourceName) {
-            if ( $jsonLoader->isJsonResourceIndexedAndFileExists($processResourceName) ) {
-                $panelResource = $jsonLoader->loadResource($processResourceName);
-                $items = $GLOBALS['myDocFunctions'][$panelResource->metadata->type]($panelResource, $jsonLoader, $breadcrumb . '.' . $chapterNumber, $level + 1);
-                if (is_array($items)) {
-                    $docs = array_merge($docs, $items);
-                } else {
-                    $docs[] = $items;
-                }
-            } else {
-                echo $panel->resource." does not exist<br>";
-            }
-            $chapterNumber += 1;
-        }
-    }
-    return $docs;
-};
-
 $GLOBALS['myTemplateFunctions']['dashboard'] = function ( $jsonResource, $jsonLoader ): BaseHTMLDashboard {
     $htmlTemplateLoader = $this->applicationBuilder->getHtmlTemplateLoader();
 
@@ -102,3 +42,5 @@ $GLOBALS['myTemplateFunctions']['dashboard'] = function ( $jsonResource, $jsonLo
 
     return $htmlDashboard;
 };
+
+
