929060364f061a46bfd4f81f46b214e57721c292
Fabio Mattei <burattino@gmail.com>
2024-12-22 11:28:51 +0100

    fixing stuff

--- src/Framework/Utils/UrlProcessor.php
+++ src/Framework/Utils/UrlProcessor.php
@@ -2,20 +2,26 @@
 
 namespace Fabiom\UglyDuckling\Framework\Utils;
 
+class UrlServices {
 
+    public static function setServerRequestURI( string $requestURI, string $pathToApp = '/' ) {
+        $request2 = str_replace( ['.html', '.pdf', '.svg'], '',$requestURI );
+        $request  = preg_replace( '/\?.*/', '', $request2 );
 
-function setServerRequestURI( string $requestURI, string $pathToApp = '/' ) {
-    $request2 = str_replace( ['.html', '.pdf', '.svg'], '',$requestURI );
-    $request  = preg_replace( '/\?.*/', '', $request2 );
+        if ( $request == '' ) throw new \Exception('General malfuction!!!');
 
-    if ( $request == '' ) throw new \Exception('General malfuction!!!');
+        # removing the first '/' from the path
+        $action = substr( $request, strlen($pathToApp) ); // explode( '/', $request );
 
-    # removing the first '/' from the path
-    $action = substr( $request, strlen($pathToApp) ); // explode( '/', $request );
+        if (validate_string( $action )) {
+            throw new \Exception('Illegal access to calculateSplittedURL!!! Unvalidated action: "' . $action . '" over request: ' . $requestURI);
+        }
 
-    if (validate_string( $action )) {
-        throw new \Exception('Illegal access to calculateSplittedURL!!! Unvalidated action: "' . $action . '" over request: ' . $requestURI);
+        return $action;
     }
 
-    return $action;
+    public static function getRequestURI(): string {
+        return filter_var((isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : NULL), FILTER_SANITIZE_URL);
+    }
+
 }
