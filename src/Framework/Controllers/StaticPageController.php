4404133b9ea90df1799521187a42030e98973f1e
Fabio Mattei <burattino@gmail.com>
2025-01-16 18:19:05 +0100

    correcting static page

--- src/Framework/Controllers/StaticPageController.php
+++ src/Framework/Controllers/StaticPageController.php
@@ -2,6 +2,13 @@
 
 namespace Fabiom\UglyDuckling\Framework\Controllers;
 
+use Fabiom\UglyDuckling\Framework\DataBase\DBConnection;
+use Fabiom\UglyDuckling\Framework\Loggers\Logger;
+use Fabiom\UglyDuckling\Framework\Mailer\BaseMailer;
+use Fabiom\UglyDuckling\Framework\SecurityCheckers\SecurityChecker;
+use Fabiom\UglyDuckling\Framework\Utils\ServerWrapper;
+use Fabiom\UglyDuckling\Framework\Utils\SessionWrapper;
+
 class StaticPageController {
 
     public string $templateFile;
@@ -20,6 +27,10 @@ class StaticPageController {
         ob_start();
         require_once 'src/Templates/' . $this->templateFile . '.php';
         return ob_end_flush();
+    }
+
+    public function makeAllPresets(DBConnection $dbconnection, Logger $logger, SecurityChecker $securityChecker, BaseMailer $mailer) {
+        // nothing to do
     }
 
 }
