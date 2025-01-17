fcce2711dbe59df38956f83cbc0d3b419cc3eca4
Fabio Mattei <burattino@gmail.com>
2025-01-17 05:37:55 +0100

    fixing class

---
+++ src/Framework/Ajax/AjaxObjectsBuilder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Fabiom\UglyDuckling\Framework\Ajax;
+
+class AjaxObjectsBuilder {
+
+    public static function createAjaxObjectForAjaxDeleteObject($type, $destination ) {
+        $myAjaxResponse = new \stdClass();
+        $myAjaxResponse->type = $type;
+        $myAjaxResponse->destination = $destination;
+        return $myAjaxResponse;
+    }
+
+    public static function createAjaxObjectForAjaxHtmlBlockObject($type, $destination, $position, $body ) {
+        $myAjaxResponse = new \stdClass();
+        $myAjaxResponse->type = $type;
+        $myAjaxResponse->destination = $destination;
+        $myAjaxResponse->position = $position;
+        $myAjaxResponse->body = $body;
+        return $myAjaxResponse;
+    }
+
+    public static function createAjaxObjectForAjaxURLBlockObject($type, $destination, $position, $url, $method ) {
+        $myAjaxResponse = new \stdClass();
+        $myAjaxResponse->type = $type;
+        $myAjaxResponse->destination = $destination;
+        $myAjaxResponse->position = $position;
+        $myAjaxResponse->url = $url;
+        $myAjaxResponse->method = $method;
+        return $myAjaxResponse;
+    }
+
+    public static function createAjaxObjectForAjaxMessageObject($type, $destination, $position, $html ) {
+        $myAjaxResponse = new \stdClass();
+        $myAjaxResponse->type = $type;
+        $myAjaxResponse->destination = $destination;
+        $myAjaxResponse->position = $position;
+        $myAjaxResponse->html = $html;
+        return $myAjaxResponse;
+    }
+
+}
