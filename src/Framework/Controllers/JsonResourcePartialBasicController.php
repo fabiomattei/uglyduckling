19026a8e6455ad16eb769346dccb52cccb32bb0d
Fabio Mattei <burattino@gmail.com>
2025-01-18 08:16:40 +0100

    inserting new controllers

---
+++ src/Framework/Controllers/JsonResourcePartialBasicController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Fabiom\UglyDuckling\Framework\Controllers;
+
+use Fabiom\UglyDuckling\Framework\Json\Parameters\BasicParameterGetter;
+use Fabiom\UglyDuckling\Framework\Status\Logics;
+
+class JsonResourcePartialBasicController extends ControllerNoCSRFTokenRenew {
+
+    const CONTROLLER_NAME = 'partial';
+
+    /**
+     * @throws GeneralException
+     */
+    public function getRequest() {
+        $this->applicationBuilder->getJsonloader()->loadIndex();
+
+        // GETTING json resource name from parameter
+        $jsonResourceName = filter_input(INPUT_POST | INPUT_GET, 'res', FILTER_UNSAFE_RAW);
+
+        // loading json resource
+        if ( ! $jsonResourceName ) {
+            echo 'missing resource name';
+            $jsonResource = new \stdClass;
+        } else {
+            if ( strlen( $jsonResourceName ) > 0 ) {
+                $jsonResource = $this->applicationBuilder->getJsonloader()->loadResource( $jsonResourceName );
+            } else {
+                $jsonResource = new \stdClass;
+            }
+        }
+
+        // if json resource was correctly loaded
+        if ( is_object( $jsonResource ) ) {
+            $this->templateFile = 'empty';
+
+            // if json resource has parameters
+            if(!isset($jsonResource->get->request) OR !isset($jsonResource->get->request->parameters)) {
+                if ( isset($jsonResource->get->sessionupdates) ) $this->pageStatus->updateSession( $jsonResource->get->sessionupdates );
+
+                $myBlocks = $this->applicationBuilder->getHTMLBlock( $jsonResource );
+                echo $myBlocks->show();
+            } else {
+                $secondGump = new \Gump;
+
+                $parametersGetter = BasicParameterGetter::parameterGetterFactory( $jsonResource, $this->applicationBuilder );
+                $validation_rules = $parametersGetter->getValidationRoules();
+                $filter_rules = $parametersGetter->getFiltersRoules();
+
+                if ( count( $validation_rules ) == 0 ) {
+                    // nothing to do
+                } else {
+                    $parms = $secondGump->sanitize( $_GET );
+                    $secondGump->validation_rules( $validation_rules );
+                    $secondGump->filter_rules( $filter_rules );
+                    $cleanGETParameters = $secondGump->run( $parms );
+                    $this->pageStatus->setGetParameters( $cleanGETParameters );
+                    $this->unvalidated_parameters = $parms;
+                    if ($secondGump->errors()) {
+                        $this->readableErrors = $secondGump->get_readable_errors(true);
+                    } else {
+                        if ( isset($jsonResource->get->sessionupdates) ) $this->pageStatus->updateSession( $jsonResource->get->sessionupdates );
+
+                        $myBlocks = $this->applicationBuilder->getHTMLBlock( $jsonResource );
+                        echo $myBlocks->show();
+                    }
+                }
+            }
+        } else {
+            echo 'resource '.$jsonResourceName.' undefined';
+        }
+    }
+
+    /**
+     * This method implements POST Request logic for all possible json resources.
+     * This means all json Resources act in the same way when there is a post request
+     */
+    public function postRequest() {
+        $this->templateFile = "empty";
+
+        $this->applicationBuilder->getJsonloader()->loadIndex();
+
+        // GETTING json resource name from parameter
+        $jsonResourceName = filter_input(INPUT_POST | INPUT_GET, 'res', FILTER_UNSAFE_RAW);
+        if ( ! $jsonResourceName ) {
+            if ( isset( $_POST['res'] ) ) {
+                $jsonResourceName = filter_var($_POST['res'], FILTER_UNSAFE_RAW);
+            }
+        }
+
+        // loading json resource
+        if ( ! $jsonResourceName ) {
+            echo 'missing resource name';
+            $jsonResource = new \stdClass;
+        } else {
+            if ( strlen( $jsonResourceName ) > 0 ) {
+                $jsonResource = $this->applicationBuilder->getJsonloader()->loadResource( $jsonResourceName );
+            } else {
+                $jsonResource = new \stdClass;
+            }
+        }
+
+        // checking parameters
+        $secondGump = new \Gump;
+        if( isset($jsonResource->post->request) AND isset($jsonResource->post->request->postparameters)) {
+            $parametersGetter = BasicParameterGetter::parameterGetterFactory( $jsonResource, $this->applicationBuilder );
+            $validation_rules = $parametersGetter->getPostValidationRoules();
+            $filter_rules = $parametersGetter->getPostFiltersRoules();
+
+            $parms = $secondGump->sanitize( array_merge($_GET, $_POST) );
+            $secondGump->validation_rules( $validation_rules );
+            $secondGump->filter_rules( $filter_rules );
+            $cleanPostParameters = $secondGump->run( $parms );
+            $this->pageStatus->setPostParameters( $cleanPostParameters );
+            $this->unvalidated_parameters = $parms;
+        }
+        if ($secondGump->errors()) {
+            $this->pageStatus->addErrors( $secondGump->get_readable_errors() );
+        } else {
+            Logics::performTransactions( $this->pageStatus, $this->applicationBuilder, $jsonResource );
+
+            Logics::performUseCases( $this->pageStatus, $this->applicationBuilder, $jsonResource );
+
+            // if resource->get->sessionupdates is set I need to update the session
+            if ( isset($this->resource->post->sessionupdates) ) $this->pageStatus->updateSession( $this->resource->post->sessionupdates );
+
+
+        }
+        echo Logics::performAjaxCallPost( $this->pageStatus, $this->applicationBuilder, $jsonResource );
+    }
+}
---
+++ src/Framework/Status/Logics.php
@@ -0,0 +1,341 @@
+<?php
+
+namespace Fabiom\UglyDuckling\Framework\Status;
+
+use Fabiom\UglyDuckling\Framework\Ajax\AjaxObjectsBuilder;
+use Fabiom\UglyDuckling\Framework\Blocks\BaseHTMLMessages;
+use Fabiom\UglyDuckling\Framework\Database\QueryExecuter;
+use Fabiom\UglyDuckling\Framework\Utils\PageStatus;
+
+class Logics {
+
+    public static function performTransactions( PageStatus $pageStatus, ApplicationBuilder $applicationBuilder, $jsonResource ): void {
+        $queryExecutor = $pageStatus->getQueryExecutor();
+        $conn = $pageStatus->getDbconnection()->getDBH();
+
+        // performing get transactions
+        if (isset($jsonResource->get->transactions)) {
+            $returnedIds = $pageStatus->getQueryReturnedValues();
+            try {
+                //$conn->beginTransaction();
+                $queryExecutor->setDBH($conn);
+                foreach ($jsonResource->get->transactions as $transaction) {
+                    $queryExecutor->setQueryStructure($transaction);
+                    if ($queryExecutor->getSqlStatmentType() == QueryExecuter::INSERT) {
+                        if (isset($transaction->label)) {
+                            $returnedIds->setValue($transaction->label, $queryExecutor->executeSql());
+                        } else {
+                            $returnedIds->setValueNoKey($queryExecutor->executeSql());
+                        }
+                    } else if ($queryExecutor->getSqlStatmentType() == QueryExecuter::SELECT) {
+                        if (isset($transaction->label)) {
+                            $returnedIds->setValue($transaction->label, $queryExecutor->executeSql());
+                        } else {
+                            $returnedIds->setValueNoKey($queryExecutor->executeSql());
+                        }
+                    } else {
+                        $queryExecutor->executeSql();
+                    }
+                }
+                //$conn->commit();
+            } catch (\PDOException $e) {
+                $pageStatus->addError("There was an error in the transaction");
+                $conn->rollBack();
+                $applicationBuilder->getLogger()->write($e->getMessage(), __FILE__, __LINE__);
+            }
+        }
+
+        // performing post transactions
+        if (isset($jsonResource->post->transactions)) {
+            $returnedIds = $pageStatus->getQueryReturnedValues();
+            try {
+                //$conn->beginTransaction();
+                $queryExecutor->setDBH($conn);
+                foreach ($jsonResource->post->transactions as $transaction) {
+                    $queryExecutor->setQueryStructure($transaction);
+                    if ($queryExecutor->getSqlStatmentType() == QueryExecuter::INSERT) {
+                        if (isset($transaction->label)) {
+                            $returnedIds->setValue($transaction->label, $queryExecutor->executeSql());
+                        } else {
+                            $returnedIds->setValueNoKey($queryExecutor->executeSql());
+                        }
+                    } else if ($queryExecutor->getSqlStatmentType() == QueryExecuter::SELECT) {
+                        if (isset($transaction->label)) {
+                            $returnedIds->setValue($transaction->label, $queryExecutor->executeSql());
+                        } else {
+                            $returnedIds->setValueNoKey($queryExecutor->executeSql());
+                        }
+                    } else {
+                        $queryExecutor->executeSql();
+                    }
+                }
+                //$conn->commit();
+            } catch (\PDOException $e) {
+                $pageStatus->addError("There was an error in the transaction");
+                $conn->rollBack();
+                $applicationBuilder->getLogger()->write($e->getMessage(), __FILE__, __LINE__);
+            }
+        }
+    }
+
+    public static function performUseCases( PageStatus $pageStatus, ApplicationBuilder $applicationBuilder, $jsonResource ): void {
+        if (isset($jsonResource->get->usecases) and is_array($jsonResource->get->usecases)) {
+            foreach ($jsonResource->get->usecases as $jsonusecase) {
+                $useCase = $pageStatus->getUseCasesIndex()->getUseCase($jsonusecase, $pageStatus, $applicationBuilder);
+                $useCase->performAction();
+            }
+        }
+        if (isset($jsonResource->post->usecases) and is_array($jsonResource->post->usecases)) {
+            foreach ($jsonResource->post->usecases as $jsonusecase) {
+                $useCase = $pageStatus->getUseCasesIndex()->getUseCase($jsonusecase, $pageStatus, $applicationBuilder);
+                $useCase->performAction();
+            }
+        }
+    }
+
+    public static function performAjaxCallGet( PageStatus $pageStatus, ApplicationBuilder $applicationBuilder, $jsonResource ): string {
+        if ( isset($jsonResource->get->ajax) and is_array($jsonResource->get->ajax)) {
+            $out = [];
+            foreach ($jsonResource->get->ajax as $ajax) {
+                if ( $ajax->type == 'delete' ) {
+                    if (is_string( $ajax->destination )) {
+                        $destination = $ajax->destination;
+                    } else if ( is_object( $ajax->destination ) ) {
+                        $destination = $pageStatus->getValue( $ajax->destination );
+                    } else {
+                        $destination = '';
+                    }
+                    $out[] = AjaxObjectsBuilder::createAjaxObjectForAjaxDeleteObject(
+                        $ajax->type,
+                        $destination
+                    );
+                }
+
+                if ( $ajax->type == 'add' OR $ajax->type == 'update') {
+                    if ( is_string( $ajax->destination ) ) {
+                        $destination = $ajax->destination;
+                    } else if ( is_object( $ajax->destination ) ) {
+                        $destination = $pageStatus->getValue( $ajax->destination );
+                    } else {
+                        $destination = '';
+                    }
+                    if ( is_string( $ajax->body ) ) {
+                        $body = $ajax->body;
+                    } else if ( is_object( $ajax->body ) ) {
+                        $body = $pageStatus->getValue( $ajax->body );
+                    } else {
+                        $body = '';
+                    }
+                    $out[] = AjaxObjectsBuilder::createAjaxObjectForAjaxHtmlBlockObject(
+                        $ajax->type,
+                        $destination,
+                        ( isset($ajax->position) AND is_string( $ajax->position ) ) ? $ajax->position : 'beforeend',
+                        $body
+                    );
+                }
+            }
+
+            return json_encode($out);
+        }
+        return '';
+    }
+
+    public static function performAjaxCallPost( PageStatus $pageStatus, ApplicationBuilder $applicationBuilder, $jsonResource ): string {
+        if ( $pageStatus->areThereErrors() ) {
+            $out = self::createErrorMessagesReadyForAjaxOutput($pageStatus, $applicationBuilder);
+
+            return json_encode($out);
+        } else if ( isset($jsonResource->post->ajaxreponses) and is_array($jsonResource->post->ajaxreponses)) {
+            $out = [];
+            foreach ($jsonResource->post->ajaxreponses as $ajax) {
+
+                if ( $ajax->type == 'delete' OR $ajax->type == 'empty' ) {
+                    if (is_string( $ajax->destination )) {
+                        $destination = $ajax->destination;
+                    } else if ( is_object( $ajax->destination ) ) {
+                        $destination = $pageStatus->getValue( $ajax->destination );
+                    } else {
+                        $destination = '';
+                    }
+
+                    $myAjaxResponse = AjaxObjectsBuilder::createAjaxObjectForAjaxDeleteObject(
+                        $ajax->type,
+                        $destination
+                    );
+
+                    $out[] = $myAjaxResponse;
+                }
+
+                if ( $ajax->type == 'append' OR $ajax->type == 'overwrite') {
+                    if ( is_string( $ajax->destination ) ) {
+                        $destination = $ajax->destination;
+                    } else if ( is_object( $ajax->destination ) ) {
+                        $destination = $pageStatus->getValue( $ajax->destination );
+                    } else {
+                        $destination = '';
+                    }
+                    if ( is_string( $ajax->body ) ) {
+                        $body = $ajax->body;
+                    } else if ( is_object( $ajax->body ) ) {
+                        $body = $pageStatus->getValue( $ajax->body );
+                    } else {
+                        $body = '';
+                    }
+
+                    $myAjaxResponse = AjaxObjectsBuilder::createAjaxObjectForAjaxHtmlBlockObject(
+                        $ajax->type,
+                        $destination,
+                        ( isset($ajax->position) AND is_string( $ajax->position ) ) ? $ajax->position : 'beforeend',
+                        $body
+                    );
+
+                    $out[] = $myAjaxResponse;
+                }
+
+                if ( $ajax->type == 'appendurl' OR $ajax->type == 'overwriteurl') {
+                    if (is_string( $ajax->destination )) {
+                        $destination = $ajax->destination;
+                    } else if ( is_object( $ajax->destination ) ) {
+                        $destination = $pageStatus->getValue( $ajax->destination );
+                    } else {
+                        $destination = '';
+                    }
+                    if (is_string( $ajax->url )) {
+                        $url = $ajax->url;
+                    } else if ( is_object( $ajax->url ) ) {
+                        $url = $applicationBuilder->getRouterContainer()->make_resource_url( $ajax->url, $pageStatus );
+                    } else {
+                        $url = '';
+                    }
+
+                    $myAjaxResponse = AjaxObjectsBuilder::createAjaxObjectForAjaxURLBlockObject(
+                        $ajax->type,
+                        $destination,
+                        ( isset($ajax->position) AND is_string( $ajax->position ) ) ? $ajax->position : 'beforeend',
+                        $url,
+                        ( isset($ajax->url) AND is_string( $ajax->url ) AND isset($ajax->method) AND is_string( $ajax->method ) ) ? $ajax->method : 'GET'
+                    );
+
+                    $out[] = $myAjaxResponse;
+                }
+
+                $out = array_merge( $out, self::createErrorMessagesReadyForAjaxOutput($pageStatus, $applicationBuilder) );
+                $out = array_merge( $out, self::createInfoMessagesReadyForAjaxOutput($pageStatus, $applicationBuilder) );
+                $out = array_merge( $out, self::createWarningMessagesReadyForAjaxOutput($pageStatus, $applicationBuilder) );
+                $out = array_merge( $out, self::createSuccessMessagesReadyForAjaxOutput($pageStatus, $applicationBuilder) );
+
+            }
+
+            return json_encode($out);
+        }
+        return '';
+    }
+
+    /**
+     * @param PageStatus $pageStatus
+     * @param ApplicationBuilder $applicationBuilder
+     * @return array|\stdClass[]
+     */
+    public static function createErrorMessagesReadyForAjaxOutput(PageStatus $pageStatus, ApplicationBuilder $applicationBuilder): array {
+        $out = [];
+        if ($pageStatus->areThereErrors()) {
+            $out = array_map(
+                function ($errorstring) use ($applicationBuilder) {
+                    $msgBlock = new BaseHTMLMessages;
+                    $msgBlock->setHtmlTemplateLoader($applicationBuilder->getHtmlTemplateLoader());
+                    $msgBlock->setError($errorstring);
+
+                    $myAjaxResponse = AjaxObjectsBuilder::createAjaxObjectForAjaxMessageObject( 'error',
+                        $jsonResource->post->error->destination ?? '#messagescontainer',
+                        $jsonResource->post->error->position ?? 'beforeend',
+                        $msgBlock->show()
+                    );
+                    return $myAjaxResponse;
+                },
+                $pageStatus->getErrors()
+            );
+        }
+        return $out;
+    }
+
+    /**
+     * @param PageStatus $pageStatus
+     * @param ApplicationBuilder $applicationBuilder
+     * @return array|\stdClass[]
+     */
+    public static function createInfoMessagesReadyForAjaxOutput(PageStatus $pageStatus, ApplicationBuilder $applicationBuilder): array {
+        $out = [];
+        if ($pageStatus->areThereInfos()) {
+            $out = array_map(
+                function ($infoString) use ($applicationBuilder) {
+                    $msgBlock = new BaseHTMLMessages;
+                    $msgBlock->setHtmlTemplateLoader($applicationBuilder->getHtmlTemplateLoader());
+                    $msgBlock->setInfo($infoString);
+
+                    $myAjaxResponse = AjaxObjectsBuilder::createAjaxObjectForAjaxMessageObject( 'info',
+                        $jsonResource->post->error->destination ?? '#messagescontainer',
+                        $jsonResource->post->error->position ?? 'beforeend',
+                        $msgBlock->show()
+                    );
+                    return $myAjaxResponse;
+                },
+                $pageStatus->getErrors()
+            );
+        }
+        return $out;
+    }
+
+    /**
+     * @param PageStatus $pageStatus
+     * @param ApplicationBuilder $applicationBuilder
+     * @return array|\stdClass[]
+     */
+    public static function createWarningMessagesReadyForAjaxOutput(PageStatus $pageStatus, ApplicationBuilder $applicationBuilder): array {
+        $out = [];
+        if ($pageStatus->areThereWarnings()) {
+            $out = array_map(
+                function ($warningString) use ($applicationBuilder) {
+                    $msgBlock = new BaseHTMLMessages;
+                    $msgBlock->setHtmlTemplateLoader($applicationBuilder->getHtmlTemplateLoader());
+                    $msgBlock->setWarning($warningString);
+
+                    $myAjaxResponse = AjaxObjectsBuilder::createAjaxObjectForAjaxMessageObject( 'warning',
+                        $jsonResource->post->error->destination ?? '#messagescontainer',
+                        $jsonResource->post->error->position ?? 'beforeend',
+                        $msgBlock->show()
+                    );
+                    return $myAjaxResponse;
+                },
+                $pageStatus->getErrors()
+            );
+        }
+        return $out;
+    }
+
+    /**
+     * @param PageStatus $pageStatus
+     * @param ApplicationBuilder $applicationBuilder
+     * @return array|\stdClass[]
+     */
+    public static function createSuccessMessagesReadyForAjaxOutput(PageStatus $pageStatus, ApplicationBuilder $applicationBuilder): array {
+        $out = [];
+        if ($pageStatus->areThereSuccesses()) {
+            $out = array_map(
+                function ($successString) use ($applicationBuilder) {
+                    $msgBlock = new BaseHTMLMessages;
+                    $msgBlock->setHtmlTemplateLoader($applicationBuilder->getHtmlTemplateLoader());
+                    $msgBlock->setSuccess($successString);
+
+                    $myAjaxResponse = AjaxObjectsBuilder::createAjaxObjectForAjaxMessageObject( 'success',
+                        $jsonResource->post->error->destination ?? '#messagescontainer',
+                        $jsonResource->post->error->position ?? 'beforeend',
+                        $msgBlock->show()
+                    );
+                    return $myAjaxResponse;
+                },
+                $pageStatus->getErrors()
+            );
+        }
+        return $out;
+    }
+}
