901622cb9a56d3c3c29defb3424b044a9157b711
Fabio Mattei <burattino@gmail.com>
2026-01-15 17:13:03 +0100

    removing favicon

--- src/Templates/applicationwithsidebar.php
+++ src/Templates/applicationwithsidebar.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="">
 	<meta name="author" content="">
-	<link rel="icon" href="../../../../favicon.ico">
 
     <!-- Page title -->
     <title><?php echo isset($this->title) ? $this->title : '' ?></title>
