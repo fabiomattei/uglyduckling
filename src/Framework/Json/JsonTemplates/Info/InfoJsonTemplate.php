c285c628afeca64f7cb5a8aab30c01e2aa0ce670
Fabio Mattei <burattino@gmail.com>
2026-02-11 14:59:30 +0100

    updating info

--- src/Framework/Json/JsonTemplates/Info/InfoJsonTemplate.php
+++ src/Framework/Json/JsonTemplates/Info/InfoJsonTemplate.php
@@ -3,6 +3,7 @@
 namespace Fabiom\UglyDuckling\Framework\Json\JsonTemplates\Info;
 
 use Fabiom\UglyDuckling\Framework\Blocks\BaseHTMLInfo;
+use Fabiom\UglyDuckling\Framework\Json\JsonTemplates\JsonDefaultTemplateFactory;
 use Fabiom\UglyDuckling\Framework\Json\JsonTemplates\JsonTemplate;
 
 /**
@@ -15,7 +16,6 @@ class InfoJsonTemplate extends JsonTemplate {
     const blocktype = 'info';
 
     public function createInfo() {
-        $dbconnection = $this->pageStatus->getDbconnection();
         $queryExecutor = $this->pageStatus->getQueryExecutor();
 
         // If there are dummy data they take precedence in order to fill the info box
@@ -24,7 +24,7 @@ class InfoJsonTemplate extends JsonTemplate {
         } else {
             // If there is a query I look for data to fill the info box,
             // if there is not query I do not
-            if ( isset($this->resource->get->query) AND isset($dbconnection) ) {
+            if ( isset($this->resource->get->query) ) {
                 $queryExecutor->setResourceName( $this->resource->name ?? 'undefined ');
                 $queryExecutor->setQueryStructure( $this->resource->get->query );
 
@@ -33,18 +33,34 @@ class InfoJsonTemplate extends JsonTemplate {
             } else {
                 $entity = new \stdClass();
             }
+
+            if ( isset($this->resource->get->titlequery) ) {
+                $queryExecutor->setResourceName( $this->resource->name ?? 'undefined ');
+                $queryExecutor->setQueryStructure( $this->resource->get->titlequery );
+
+                $result = $queryExecutor->executeSql();
+                $titleentity = $result->fetch();
+            } else {
+                $titleentity = new \stdClass();
+            }
+        }
+
+		$infoBlock = new BaseHTMLInfo;
+        if ( isset( $this->resource->get->titlequery ) ) {
+            $this->pageStatus->setLastEntity($titleentity);
+            $value = $this->pageStatus->getValue($this->resource->get->info->title);
+            $infoBlock->setTitle($value);
+        } else {
+            $infoBlock->setTitle($this->resource->get->info->title ?? '');
         }
 
         $this->pageStatus->setLastEntity($entity);
+        $fieldRows = [];
 
-		$infoBlock = new BaseHTMLInfo;
-		$infoBlock->setTitle($this->resource->get->info->title ?? '');
-		$fieldRows = array();
-		
-		foreach ($this->resource->get->info->fields as $field) {
-			if( !array_key_exists(($field->row ?? 1), $fieldRows) ) $fieldRows[$field->row] = array();
-			$fieldRows[($field->row ?? 1)][] = $field;
-		}
+        foreach ($this->resource->get->info->fields as $field) {
+            if( !array_key_exists(($field->row ?? 1), $fieldRows) ) $fieldRows[$field->row] = array();
+            $fieldRows[($field->row ?? 1)][] = $field;
+        }
 		
         $rowcounter = 1;
 		foreach ($fieldRows as $row) {
@@ -54,19 +70,33 @@ class InfoJsonTemplate extends JsonTemplate {
                 if ($field->type === 'textfield') {
                     $infoBlock->addTextField($field->label, $value, $field->width ?? '', $field->cssclass ?? '');
                 }
-                if ($field->type === 'textarea') {
+                elseif ($field->type === 'textarea') {
                     $infoBlock->addTextAreaField($field->label, $value, $field->width ?? '', $field->cssclass ?? '');
                 }
-                if ($field->type === 'currency') {
+                elseif ($field->type === 'currency') {
                     $infoBlock->addCurrencyField($field->label, $value, $field->width ?? '', $field->cssclass ?? '');
                 }
-                if ($field->type === 'date') {
+                elseif ($field->type === 'date') {
                     $infoBlock->addDateField($field->label, $value, $field->width ?? '', $field->cssclass ?? '');
                 }
 			}
 			$infoBlock->closeRow('row '.$rowcounter);
             $rowcounter++;
 		}
+        // $topButtons = '';
+        // if (isset($this->resource->get->info->topbuttons) AND is_array($this->resource->get->info->topbuttons)) {
+        //     foreach ($this->resource->get->info->topbuttons as $button) {
+        //         $topButtons .= JsonDefaultTemplateFactory::getHTMLTag( $button, $this->pageStatus, $this->jsonTabTemplates );
+        //     }
+        // }
+        // $infoBlock->setTopButtons($topButtons);
+        // $bottomButtons = '';
+        // if (isset($this->resource->get->info->bottombuttons) AND is_array($this->resource->get->info->bottombuttons)) {
+        //     foreach ($this->resource->get->info->bottombuttons as $button) {
+        //         $bottomButtons .= JsonDefaultTemplateFactory::getHTMLTag( $button, $this->pageStatus, $this->jsonTabTemplates );
+        //     }
+        // }
+        // $infoBlock->setBottomButtons($bottomButtons);
         return $infoBlock;
     }
 
