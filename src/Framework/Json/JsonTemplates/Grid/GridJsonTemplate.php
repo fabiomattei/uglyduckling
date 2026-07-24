0a816becea1d2be5675b32dfdc5b86c91198ccaa
Fabio Mattei <burattino@gmail.com>
2026-07-24 19:02:03 +0200

    Update GridJsonTemplate.php

--- src/Framework/Json/JsonTemplates/Grid/GridJsonTemplate.php
+++ src/Framework/Json/JsonTemplates/Grid/GridJsonTemplate.php
@@ -11,6 +11,7 @@ namespace Fabiom\UglyDuckling\Framework\Json\JsonTemplates\Grid;
 use Fabiom\UglyDuckling\Framework\Json\JsonTemplates\JsonDefaultTemplateFactory;
 use Fabiom\UglyDuckling\Framework\Json\JsonTemplates\JsonTemplate;
 use Fabiom\UglyDuckling\Framework\Blocks\BaseHTMLGrid;
+use Fabiom\UglyDuckling\Framework\Routing\RouteTable;
 
 class GridJsonTemplate extends JsonTemplate {
 
@@ -23,14 +24,21 @@ class GridJsonTemplate extends JsonTemplate {
      * @return BaseHTMLGrid
      */
     public function createHTMLBlock(): BaseHTMLGrid {
+        // A panel whose resource the session's group can't open is dropped entirely,
+        // not just skipped when rendering - so it's never part of the interface at all.
+        $panels = array_values(array_filter(
+            $this->resource->panels,
+            fn($panel) => RouteTable::isVisible($panel->resource)
+        ));
+
         $gridBlocks = [];
-        foreach ($this->resource->panels as $panel) {
+        foreach ($panels as $panel) {
             $gridBlocks[$panel->id] = JsonDefaultTemplateFactory::getHTMLBlock($this->resourcesIndex, $this->jsonResourceTemplates, $this->jsonTabTemplates, $this->pageStatus, $panel->resource);
         }
 
         $grid = new BaseHTMLGrid;
         $grid->setBlocks($gridBlocks);
-        $grid->setPanels($this->resource->panels);
+        $grid->setPanels($panels);
         $grid->setCssClass($this->resource->cssclass);
 
         return $grid;
